5a91ed0685378527bc8bea45223d6c4565e28c2c
Andreas Lillebø <git@andreaslillebo.com>
2021-07-21 14:35:11 +0900

    Display header graphic on more screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,26 +64,34 @@ module.exports = {
       },
       lineHeight: {
         '20': '5rem',
-       }
+      },
+      fontFamily: {
+        sans: [
+          'Work Sans',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"'
+        ],
+      },
     },
-    fontFamily: {
-      sans: [
-        'Work Sans',
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        '"Noto Sans"',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"'
-      ],
+    screens: {
+      'xs': '420px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
   },
   plugins: [
